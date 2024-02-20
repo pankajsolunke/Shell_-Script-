3969fd6aef67b049cbe9c964c16763c0749ac291
pankajsolunke <pankajsolunke8506@gmail.com>
2024-02-20 23:12:41 +0530

    add a if elif program

---
+++ ifelifstr.sh
@@ -0,0 +1,12 @@
+echo -e "What is your name?"
+read name
+if ["name"="ironman"];
+then
+        echo -e "come to my office"
+elif ["$name" = "pankaj"];
+then
+        echo -e "come to my home"
+else
+        echo "sorry we do not have message"
+fi
+Ifelifstr.sh
